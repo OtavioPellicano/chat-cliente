de44aedf94d4713af484edd79d0344e5a7ae65e0
OtavioPellicano <otavio.pellicano@gmail.com>
2018-07-03 05:26:27 -0300

    Agora existe uma memória do último host e porta escolhido

--- configuracoes.cpp
+++ configuracoes.cpp
@@ -7,6 +7,9 @@ Configuracoes::Configuracoes(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    setup();
+    carregarHostPorta();
+
 }
 
 Configuracoes::~Configuracoes()
@@ -16,6 +19,106 @@ Configuracoes::~Configuracoes()
 
 void Configuracoes::on_pushButton_ok_clicked()
 {
-    emit pushButtonOkClicked(ui->lineEdit_host->text(), ui->spinBox->value());
+    setHost(ui->lineEdit_host->text());
+    setPorta(QString::number(ui->spinBox->value()));
+    salvarHostPorta();
+
+    emit pushButtonOkClicked(host(), porta().toInt());
     this->close();
+}
+
+QString Configuracoes::sep() const
+{
+    return mSep;
+}
+
+QString Configuracoes::nomeArq() const
+{
+    return mNomeArq;
+}
+
+QDir Configuracoes::dirConf() const
+{
+    return mDirConf;
+}
+
+QString Configuracoes::porta() const
+{
+    return mPorta;
+}
+
+void Configuracoes::setPorta(const QString &porta)
+{
+    mPorta = porta;
+}
+
+void Configuracoes::setup()
+{
+    mDirConf.mkdir("confg");
+    mDirConf.cd("confg");
+    mNomeArq =  "confg.csv";
+}
+
+void Configuracoes::carregarHostPorta()
+{
+    using namespace std;
+
+    ifstream arq;
+    QStringList strCsv;
+    string str;
+
+    arq.open(dirConf().absoluteFilePath(nomeArq()).toStdString());
+    if(arq.is_open())
+    {
+        for(size_t i = 0; getline(arq, str); ++i)
+        {
+            if(i != 0)
+            {
+                strCsv = QString::fromStdString(str).split(sep());
+                setHost(strCsv[0]);
+                setPorta(strCsv[1]);
+                break;
+            }
+        }
+        arq.close();
+    }
+    else
+    {
+        setHost("127.0.0.1");
+        setPorta("1312");
+    }
+
+    ui->lineEdit_host->setText(host());
+    ui->spinBox->setValue(porta().toInt());
+
+}
+
+void Configuracoes::salvarHostPorta()
+{
+    using namespace std;
+    ofstream arq(dirConf().absoluteFilePath(nomeArq()).toStdString());
+
+    if(arq.is_open())
+    {
+        arq << QString("%1%2%3").arg("host").arg(sep()).arg("porta").toStdString() << endl;
+        arq << QString("%1%2%3").arg(host()).arg(sep()).arg(porta()).toStdString() << endl;
+        arq.close();
+        qDebug() << "arquivo confg.csv salvo com sucesso";
+    }
+    else
+    {
+        qDebug() << "erro ao salvar confg.csv";
+    }
+
+
+}
+
+QString Configuracoes::host() const
+{
+    return mHost;
+}
+
+void Configuracoes::setHost(const QString &host)
+{
+    mHost = host;
 }
--- configuracoes.h
+++ configuracoes.h
@@ -2,6 +2,10 @@
 #define CONFIGURACOES_H
 
 #include <QDialog>
+#include <QDir>
+#include <fstream>
+#include <QStringList>
+#include <QDebug>
 
 namespace Ui {
 class Configuracoes;
@@ -15,6 +19,25 @@ public:
     explicit Configuracoes(QWidget *parent = 0);
     ~Configuracoes();
 
+private:
+
+    QString sep() const;
+
+    QDir dirConf() const;
+
+    QString nomeArq() const;
+
+    QString host() const;
+    void setHost(const QString &host);
+
+    QString porta() const;
+    void setPorta(const QString &porta);
+
+    void setup();
+
+    void carregarHostPorta();
+    void salvarHostPorta();
+
 signals:
     void pushButtonOkClicked(QString host, int porta);
 
@@ -23,6 +46,14 @@ private slots:
 
 private:
     Ui::Configuracoes *ui;
+
+    QString mHost;
+    QString mPorta;
+
+    QDir mDirConf;
+    QString mNomeArq;
+    QString mSep = ";";
+
 };
 
 #endif // CONFIGURACOES_H
